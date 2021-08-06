f377452bd447ad6a5c80f0fc53a3a6306ecab132
wtnb93 <wtnb93@users.noreply.github.com>
2021-08-06 20:46:38 +0900

    WIP

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+export interface options {}
+
+/**
+ * in ES module
+ * const waitForScrollCotent = require('waitforscrollcontent')
+ * 
+ * in Common JS
+ * import { waitForScrollContent } from 'waitforscrollcontent'
+ */
+export function waitForScrollContent() {
+  console.log('hello')
+}
